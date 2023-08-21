2e59a493e10ed2a4d04818de50d98c997e3a8943
Максим Халяпин <nekokat89@gmail.com>
2023-08-21 11:24:21 +0300

    Добавьте файлы проекта.

---
+++ Befunge.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Befunge_Interpreter
+{
+    public class BefungeInterpreter
+    {
+
+        public string Interpret(string code)
+        {
+            return string.Empty;
+        }
+    }
+}
