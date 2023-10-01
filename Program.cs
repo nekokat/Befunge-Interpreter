df0bd52cda65c2331968a87ff37e4828fe50017d
Максим Халяпин <nekokat89@gmail.com>
2023-10-01 14:02:31 +0300

    upd

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//TODO: Create Interpritator
+using Befunge_Interpreter;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello Befunge World!");
+
+        using (StreamWriter sw = new StreamWriter("befunge_output.txt"))
+        {
+            BefungeInterpreter.Interpret(">& : 2v2:    <\n      `      /\n      !      2 \n@,\"t\" _  :2%!|\n             >\"f\",@");
+        }
+    }
+}
