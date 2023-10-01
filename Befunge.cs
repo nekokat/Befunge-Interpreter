8e5acaa4f12437b6f4048eaf807ede0599f8c49b
Максим Халяпин <nekokat89@gmail.com>
2023-10-01 19:44:24 +0300

    update

--- Befunge.cs
+++ Befunge.cs
@@ -8,10 +8,103 @@ namespace Befunge_Interpreter
 {
     public class BefungeInterpreter
     {
+        private static string[] data;
+        private static Stack<string> stack = new Stack<string>();
+        private Stack<string> ASCIIstring = new Stack<string>();
+        private static int rows = 0;
+        private static int columns = 0;
+        private bool ASCIIMode = false;
 
-        public string Interpret(string code)
+        private static void ToData(string code)
         {
-            return string.Empty;
+            data = code.Split("\n");
+        }
+
+        public static string Interpret(string code)
+        {
+            ToData(code);
+
+            char item = data[rows][columns];
+
+            while (true)
+            {
+                if (columns == data[0].Length)
+                {
+                    columns = 0;
+                    rows += 1;
+                }
+                if (item == '@')
+                {
+                    break;
+                }
+
+            }
+
+            return string.Join("", stack);
+        }
+
+        /// <summary>
+        /// If char is number than push this number onto the stack.
+        /// </summary>
+        private void IsNumber(char item)
+        {
+
+        }
+
+        private void Addition()
+        {
+
+        }
+
+        private void Subtraction()
+        {
+
+        }
+
+        private void Multiplication()
+        {
+
+        }
+
+        private void Division()
+        {
+
+        }
+
+        private void Modulo()
+        {
+
+        }
+
+        private void LogicalNot()
+        {
+
+        }
+        private void GreaterThan()
+        {
+
+        }
+
+        private void Up()
+        {
+            rows -= 1;
+        }
+        private void Down()
+        {
+            rows += 1;
+        }
+        private void Left()
+        {
+            columns -= 1;
+        }
+
+        private void Rigth()
+        {
+            columns += 1;
+        }
+        private void StringMode()
+        {
+            ASCIIMode = !ASCIIMode;
         }
     }
 }
