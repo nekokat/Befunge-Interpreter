05719f666ea09b5d887741a9df0c44ac97479a5e
Максим Халяпин <nekokat89@gmail.com>
2023-10-02 11:40:00 +0300

    upd

--- Befunge.cs
+++ Befunge.cs
@@ -8,103 +8,114 @@ namespace Befunge_Interpreter
 {
     public class BefungeInterpreter
     {
-        private static string[] data;
-        private static Stack<string> stack = new Stack<string>();
-        private Stack<string> ASCIIstring = new Stack<string>();
-        private static int rows = 0;
-        private static int columns = 0;
-        private bool ASCIIMode = false;
+        private string[] data;
 
-        private static void ToData(string code)
+        public BefungeInterpreter()
+        {
+            Strings = new Stack<string>();
+            ASCIIstring = new Stack<string>();
+            Row = 0;
+            Col = 0;
+            ASCIIMode = false;
+        }
+
+        string[] Data { get => data; set => data = value; }
+        int Row { get; set; }
+        int Col { get; set; }
+        Stack<string> Strings { get; set; }
+        Stack<string> ASCIIstring { get; set; }
+        bool ASCIIMode { get; set; }
+
+        void ToData(string code)
         {
             data = code.Split("\n");
         }
 
-        public static string Interpret(string code)
+        public string Interpret(string code)
         {
             ToData(code);
 
-            char item = data[rows][columns];
+            char item = Data[Row][Col];
 
             while (true)
             {
-                columns += 1;
+                Col += 1;
 
                 if (item == '@')
                 {
                     break;
                 }
 
-                if (columns == data[0].Length)
+                if (Col == data[0].Length)
                 {
-                    columns = 0;
-                    rows += 1;
+                    Col = 0;
+                    Row += 1;
                 }
             }
 
-            return string.Join("", stack);
+            return string.Join("", Strings);
         }
 
         /// <summary>
         /// If char is number than push this number onto the stack.
         /// </summary>
-        private static void IsNumber(char item)
+        void IsNumber(char item)
         {
 
         }
 
-        private static void Addition()
+        void Addition()
         {
 
         }
 
-        private static void Subtraction()
+        void Subtraction()
         {
 
         }
 
-        private static void Multiplication()
+        void Multiplication()
         {
 
         }
 
-        private static void Division()
+        void Division()
         {
 
         }
 
-        private static void Modulo()
+        void Modulo()
         {
 
         }
 
-        private static void LogicalNot()
+        void LogicalNot()
         {
 
         }
-        private static void GreaterThan()
+        void GreaterThan()
         {
 
         }
 
-        private static void Up()
+        void Up()
         {
-            rows -= 1;
+            Row -= 1;
         }
-        private static void Down()
+        void Down()
         {
-            rows += 1;
+            Row += 1;
         }
-        private static void Left()
+        void Left()
         {
-            columns -= 1;
+            Col -= 1;
         }
 
-        private static void Rigth()
+        void Rigth()
         {
-            columns += 1;
+            Col += 1;
         }
-        private static void StringMode()
+        void StringMode()
         {
             ASCIIMode = !ASCIIMode;
         }
--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ class Program
 
         using (StreamWriter sw = new StreamWriter("befunge_output.txt"))
         {
-            BefungeInterpreter.Interpret(">& : 2v2:    <\n      `      /\n      !      2 \n@,\"t\" _  :2%!|\n             >\"f\",@");
+            BefungeInterpreter().Interpret(">& : 2v2:    <\n      `      /\n      !      2 \n@,\"t\" _  :2%!|\n             >\"f\",@");
         }
     }
 }
