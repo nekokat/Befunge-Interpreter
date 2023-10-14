94544758eb9548cf9f3db8dd14b5cdad1713f212
Максим Халяпин <nekokat89@gmail.com>
2023-10-14 19:47:17 +0300

    upd

--- Program.cs
+++ Program.cs
@@ -1,15 +1,18 @@
 ﻿using System;
 using Befunge_Interpreter;
+using CLI;
 
 class Program
 {
     static void Main(string[] args)
     {
+        var parameters = new Parsing(args);
+        Console.WriteLine(string.Join(" ; ", args));
         //TODO: https://learn.microsoft.com/ru-ru/dotnet/standard/commandline/define-commands#define-options
-        using (StreamWriter sw = new("befunge_output.txt"))
+        using (StreamWriter sw = new(parameters.Output))
         {
             BefungeInterpreter bi = new();
-            string code = "0>:00p58*`#@_0>:01p78vv$$<\r\n@^+1g00,+55_v# !`\\+*9<>4v$\r\n@v30p20\"?~^\"< ^+1g10,+*8<$\r\n@>p0\\>\\::*::882**02g*0v >^\r\n`*:*\" d\":+*:-*\"[Z\"+g3 < |<\r\nv-*\"[Z\"+g30*g20**288\\--\\<#\r\n>2**5#>8*:*/00g\"P\"*58*:*v^\r\nv*288 p20/**288:+*\"[Z\"+-<:\r\n>*%03 p58*:*/01g\"3\"* v>::^\r\n   \\_^#!:-1\\+-*2*:*85<^";
+            string code = parameters.Input;
             sw.Write(bi.Interpret(code));
         }
         
