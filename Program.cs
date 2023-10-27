29bed9d611e066e115317a2f1fe6f46035371df9
Максим Халяпин <nekokat89@gmail.com>
2023-10-27 12:27:08 +0300

    update

--- Program.cs
+++ Program.cs
@@ -10,17 +10,32 @@ class Program
         var parameters = new Parsing(args);
         Console.WriteLine(string.Join(" ; ", args));
         CLI cli = new();
+
         Group readgrp = new()
         {
             Name = "read",
             Description = "Read from",
             Actions = new() {
-                { "--file", new Command("ReadFromFile", "Read data from file"){ Actions = () => { }, Aliases = new string[]{ "-f", "/f"} } },
-                { "--string", new Command("ReadFromString", "Read data from string"){ Actions = () => { }, Aliases = new string[]{ "-s", "/s"} } }
+                { "--file", new Command("--file", "Read data from file"){ Actions = () => { }, Aliases = new string[]{ "-f", "/f"} } },
+                { "--path", new Command("--path", "file path"){ Actions = () => { }, Aliases = new string[]{ "-p", "/p"} } },
+                { "--string", new Command("--string", "Read data from string"){ Actions = () => { }, Aliases = new string[]{ "-s", "/s"} } }
+            }
+        };
+
+        Group outgrp = new()
+        {
+            Name = "Output",
+            Description = "Output to",
+            Actions = new() {
+                { "--output", new Command("--output", "Output data to file"){ Actions = () => { }, Aliases = new string[]{ "-o", "/o"} } },
+                { "--path", new Command("--path", "file path"){ Actions = () => { }, Aliases = new string[]{ "-p", "/p"} } },
+                { "--terminal", new Command("--string", "Output data in terminal"){ Actions = () => { }, Aliases = new string[]{ "-t", "/t"} } }
             }
         };
 
         cli.Add(readgrp);
+        cli.Add(outgrp);
+
         //TODO: https://learn.microsoft.com/ru-ru/dotnet/standard/commandline/define-commands#define-options
 
         using (StreamWriter sw = new(parameters.Output))
