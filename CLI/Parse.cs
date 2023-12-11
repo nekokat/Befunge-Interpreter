450b970a567c8016244e98e60640d5a4d73852ab
Максим Халяпин <nekokat89@gmail.com>
2023-12-11 12:49:38 +0300

    add comments

--- CLI/Parse.cs
+++ CLI/Parse.cs
@@ -2,15 +2,29 @@
 {
     public class Parse
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
         public Parse(string[] args)
         {
             Input = args[0];
             Output = args[1];
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
         public Parse(){}
 
+        /// <summary>
+        /// 
+        /// </summary>
         public string Input { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
         public string Output { get; set; }
     }
 }
